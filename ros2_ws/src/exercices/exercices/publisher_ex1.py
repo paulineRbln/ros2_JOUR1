fffbe0e6678130b59171c76dcae675b737c5707c
paulineRblin <problin@ensc.fr>
2025-10-27 11:12:53 +0100

    ex1 fini

---
+++ ros2_ws/src/exercices/exercices/publisher_ex1.py
@@ -0,0 +1,41 @@
+import rclpy
+from rclpy.node import Node
+import socket
+import os
+
+from std_msgs.msg import String
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+        nom_ordi = socket.gethostname()
+        domain_id = os.getenv('ROS_DOMAIN_ID', '0')
+        msg.data = f'Nom ordi: {nom_ordi}, Domain ID: {domain_id}'
+        
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
